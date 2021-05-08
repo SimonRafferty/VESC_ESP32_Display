970024e2e1ce7b0c96bb551d6ca2b27a76770e67
SimonRafferty <simon1@psi-design.co.uk>
2021-05-08 12:05:13 +0100

    Add files via upload
    Made a few things work better

--- VESC_Display.ino
+++ VESC_Display.ino
@@ -37,7 +37,14 @@ TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 /** Initiate VescUart class */
 VescUart UART;
 
-int rpm;
+//Change these values to suit your setup
+int Poles = 46;                  //Usually 46 for hub motor
+float WheelDia = 0.51;           //Wheel diameter in m
+float GearReduction = 1;         //reduction ratio. 1 for direct drive. Otherwise motor pulley diameter / Wheel pulley diameter.
+float BatteryCells = 13;         //Number of cells in Battery
+
+
+float rpm;
 float voltage;
 float current;
 int power;
@@ -60,7 +67,7 @@ void setup() {
   /** Setup UART port On TTGO Display, you have to assign the pins. 25(Tx) 26(Rx) in this case */
   Serial2.begin(19200, SERIAL_8N1, 25, 26);
   /** Define which ports to use as UART */
- UART.setSerialPort(&Serial2);
+  UART.setSerialPort(&Serial2);
 
   tft.init();
   tft.setRotation(1);
@@ -85,21 +92,38 @@ void loop() {
 ////////// Read values //////////  
  if ( UART.getVescValues() ) {
 
-  rpm = (UART.data.rpm)/23;          // The '23' is the number of pole pairs in the motor. Most motors have 14 poles, therefore 7 pole pairs
-  voltage = (UART.data.inpVoltage);
-  current = (UART.data.avgInputCurrent);
+  rpm = UART.data.rpm / (Poles / 2);                                // UART.data.rpm returns cRPM.  Divide by no of pole pairs in the motor for actual. 
+  voltage = (UART.data.inpVoltage);                                 //Battery Voltage
+  current = (UART.data.avgInputCurrent);                            //Current Draw
   power = voltage*current;
-  amphour = (UART.data.ampHours);
-  watthour = amphour*voltage;
-  tach = (UART.data.tachometerAbs)/138;          // The '138' is the number of motor poles multiplied by 3
-  distance = tach*3.142*(1/1609)*0.51*(16/185);          // Motor RPM x Pi x (1 / meters in a mile or km) x Wheel diameter x (motor pulley / wheelpulley)
-  velocity = rpm*3.142*(60/1609)*0.51*(16/185);          // Motor RPM x Pi x (seconds in a minute / meters in a mile) x Wheel diameter x (motor pulley / wheelpulley)
-  batpercentage = ((voltage-38.4)/12)*100;          // ((Battery voltage - minimum voltage) / number of cells) x 100
+  amphour = (UART.data.ampHours);                                   //This doesn't seem to do anything!
+  watthour = amphour*voltage;                                       //Likewise
+  distance = rpm*3.142*(1.0/1609.0)*WheelDia*GearReduction;         // Motor RPM x Pi x (1 / meters in a mile or km) x Wheel diameter x (motor pulley / wheelpulley)
+  velocity = rpm*3.142*(60.0/1609.0)*WheelDia*GearReduction;        // Motor RPM x Pi x (seconds in a minute / meters in a mile) x Wheel diameter x (motor pulley / wheelpulley)
+  batpercentage = ((voltage-(3.0*BatteryCells)/BatteryCells)*100;   // Based on a minimum of 3V per cell
 
+  /*
+  //Debug
+  Serial.print("RPM="); Serial.print(rpm);
+  Serial.print("|V="); Serial.print(voltage);
+  Serial.print("|I="); Serial.print(current);
+  Serial.print("|Ah="); Serial.print(amphour);
+  Serial.print("|Vel-"); Serial.print(velocity);
+  Serial.print("|Dist-"); Serial.print(distance);
+  Serial.println();
+  */
+  
 ////////// Filter //////////
   // calculate the estimated value with Kalman Filter
   float powerfiltered = Filter1.updateEstimate(power);
-  
+
+
+
+///////// Adjust the below to show apparently legal values //////////
+  //if(velocity>10) velocity = (velocity-10)/3+10  //Accurate 0-10mph, reduced 10-15mph so max 25mph shows as 15mph
+  //powerfiltered = powerfiltered / 6 //Read 0 to 250W
+
+
 
 ////////// LCD //////////
   
@@ -110,13 +134,13 @@ void loop() {
   tft.drawRightString(String(int(velocity)), 80, 3, 7);
   tft.drawString("mph", 85, 3, 2);
 
-  //Change Battery Percentage colour
+  //Change Battery Percentage colour based on value
   tft.setTextColor(Red); 
   if(batpercentage>30) tft.setTextColor(Orange); 
   if(batpercentage>50) tft.setTextColor(Yellow); 
   if(batpercentage>80) tft.setTextColor(Green); 
 
-  tft.setTextColor(Yellow); // Note: the new fonts do not draw the background colour
+  //tft.setTextColor(Yellow); // Note: the new fonts do not draw the background colour
   tft.drawRightString(String(int(batpercentage)), 220, 3, 7);
   tft.drawString("%", 223, 3, 2);
 
@@ -136,28 +160,11 @@ void loop() {
   tft.setTextColor(Orange); // Note: the new fonts do not draw the background colour
   tft.drawCentreString("No Data!", Disp_H/2, 10, 4);
 
-  /*
-  //Screen layout test data
-  tft.setTextColor(Yellow); // Note: the new fonts do not draw the background colour
-  tft.drawRightString("28", 80, 3, 7);
-  tft.drawString("mph", 85, 3, 2);
 
-  tft.setTextColor(Green); // Note: the new fonts do not draw the background colour
-  tft.drawRightString("100", 220, 3, 7);
-  tft.drawString("%", 223, 3, 2);
-
-// Second line
-  tft.setTextColor(Orange); // Note: the new fonts do not draw the background colour
-  tft.drawRightString("1378", 120, 80, 7);
-  tft.drawString("W", 132, 80, 2);
-
-  tft.setTextColor(Red); // Note: the new fonts do not draw the background colour
-  tft.drawRightString("54", 225, 80, 7);
-  tft.drawString("V", 228, 80, 2);
-  */
   }
  
-    
-  delay(1000);
+  //To reduce screen flicker, increase value.
+  //I ought to re-write it so it draws over the rext in black, which would flicker much less  
+  delay(500);
   
 }
